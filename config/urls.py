7d042bf738e426eb2ff4d6d834152ebff6452579
BaikNadia <baikowa.nadia@gmail.com>
2025-09-02 12:58:15 +0300

    change urls.py

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),  # Вход, выход, восстановление
+    path('accounts/register/', TemplateView.as_view(template_name='registration/register.html'), name='register'),
     path('', include('mailing.urls')),
 ]
+
+
